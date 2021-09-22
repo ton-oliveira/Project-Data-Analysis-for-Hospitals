aa700bc5f6419ab790c0619c6340b087d6e40ee1
Everton Oliveira <everton.oliveira@tuta.io>
2021-09-21 21:36:39 -0300

    Merge them

--- analysis.py
+++ analysis.py
@@ -1,14 +1,20 @@
 import pandas as pd
 
 
-# write your code here
 pd.set_option('display.max_columns', 8)
-
+# Read CSV files with datasets
 df_general = pd.read_csv('test/general.csv')
-print(df_general.head(20))
-
 df_prenatal = pd.read_csv('test/prenatal.csv')
-print(df_prenatal.head(20))
-
 df_sport = pd.read_csv('test/sports.csv')
-print(df_sport.head(20))
+
+# Change the column names
+df_prenatal = df_prenatal.rename(columns={'HOSPITAL': 'hospital', 'Sex': 'gender', 'ultrasound': 'ultrasound'})
+df_sport = df_sport.rename(columns={'Hospital': 'hospital', 'Male/female': 'gender'})
+
+# Merge the data frames into one.
+df_union = pd.concat([df_general, df_prenatal, df_sport], ignore_index=True)
+# Delete the Unnamed: 0 column
+df_union.drop(columns='Unnamed: 0', inplace=True)
+
+# Print random 20 rows of the resulting data frame
+print(df_union.sample(n=20, random_state=30, ignore_index=False))
